bd5ba11a484182478490b30fc2c1ef6aad0275d0
Franklin Oguama <turtle.foguama@gmail.com>
2021-01-18 01:28:17 +0000

    I'm starting things off with NodeJS

---
+++ index.js
@@ -0,0 +1,14 @@
+var random = (array) => {Math.floor(Math.random() * array.length);}
+class Game
+{
+	constructor()
+	{
+		this.name = "Felix";
+	}
+	startGame()
+	{
+		// ['']
+	}
+}
+
+console.log('Test')
